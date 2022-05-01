605497c7ec9ff2dd1bfd49fa9de7428d9fde5ba3
vidyathota567 <104410783+vidyathota567@users.noreply.github.com>
2022-05-01 12:53:07 -0700

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -3,8 +3,11 @@ package com.test;
 public class FirstProgram {
 
 	public static void main(String[] args) {
+		int x,y;
+		x=y=2;
 		System.out.println("Welcome to Java");
 		System.out.println("Github");
+		System.out.println("Add x and y:"+(x+y));
 
 	}
 
